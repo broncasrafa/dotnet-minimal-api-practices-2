96a0e75b76c7506a566f322ee919a141892d38d4
Rafael Francisco <broncasrafa@gmail.com>
2024-11-19 16:23:25 -0300

    adding base exception class

---
+++ src/Student.Domain/Exceptions/Common/BaseException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Student.Domain.Exceptions.Common;
+
+internal abstract class BaseException : Exception
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public BaseException(string message, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
