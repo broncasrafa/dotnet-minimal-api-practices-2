2ef4b5cd5a9281dd1ce60ea3c853c72b2e11c450
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 11:08:18 -0300

    👉🏾 refactor in student repository to use generic repository class

--- src/Student.Domain/Interfaces/Repositories/IStudentRepository.cs
+++ src/Student.Domain/Interfaces/Repositories/IStudentRepository.cs
@@ -1,17 +1,9 @@
 ﻿using System.Linq.Expressions;
+using Student.Domain.Interfaces.Repositories.Common;
 
 namespace Student.Domain.Interfaces.Repositories;
 
-public interface IStudentRepository
+public interface IStudentRepository : IGenericRepository<Entities.Student>
 {
-    Task<IEnumerable<Entities.Student>> GetAllAsync();
-    Task<Entities.Student> GetByIdAsync(int id);
     Task<Entities.Student> GetStudentDetailsAsync(int id);
-    Task<Entities.Student> GetByAsync(Expression<Func<Entities.Student, bool>> predicate);
-    Task<IEnumerable<Entities.Student>> FindByAsync(Expression<Func<Entities.Student, bool>> predicate);
-    Task<Entities.Student> SingleOrDefaultAsync(Expression<Func<Entities.Student, bool>> predicate);
-    Task<Entities.Student> FirstOrDefaultAsync(Expression<Func<Entities.Student, bool>> predicate);
-    Task<Entities.Student> InsertAsync(Entities.Student request);
-    Task<Entities.Student> UpdateAsync(Entities.Student request);
-    Task DeleteAsync(Entities.Student entity);
 }
--- src/Student.Infrastructure/Persistence/Repositories/StudentRepository.cs
+++ src/Student.Infrastructure/Persistence/Repositories/StudentRepository.cs
@@ -1,49 +1,16 @@
-﻿using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Student.Domain.Interfaces.Repositories;
 using Student.Infrastructure.Persistence.Context;
+using Student.Infrastructure.Persistence.Repositories.Common;
 
 namespace Student.Infrastructure.Persistence.Repositories;
 
-internal class StudentRepository(ApplicationDbContext context) : IStudentRepository
+internal class StudentRepository : GenericRepository<Domain.Entities.Student>, IStudentRepository
 {
-    public async Task<IEnumerable<Domain.Entities.Student>> GetAllAsync() 
-        => await context.Students.ToListAsync();
-
-    public async Task<Domain.Entities.Student> GetByIdAsync(int id) 
-        => await context.Students.FindAsync(id);
+    public StudentRepository(ApplicationDbContext context) : base(context)
+    {
+    }
 
     public async Task<Domain.Entities.Student> GetStudentDetailsAsync(int id)
-        => await context.Students.Include(q => q.Enrollments).ThenInclude(q => q.Course).SingleOrDefaultAsync(c => c.Id == id);
-
-    public async Task<Domain.Entities.Student> GetByAsync(Expression<Func<Domain.Entities.Student, bool>> predicate) 
-        => await context.Students.FirstOrDefaultAsync(predicate);
-
-    public async Task<IEnumerable<Domain.Entities.Student>> FindByAsync(Expression<Func<Domain.Entities.Student, bool>> predicate) 
-        => await context.Students.Where(predicate).ToListAsync();
-
-    public async Task<Domain.Entities.Student> SingleOrDefaultAsync(Expression<Func<Domain.Entities.Student, bool>> predicate) 
-        => await context.Students.SingleOrDefaultAsync(predicate);
-
-    public async Task<Domain.Entities.Student> FirstOrDefaultAsync(Expression<Func<Domain.Entities.Student, bool>> predicate) 
-        => await context.Students.FirstOrDefaultAsync(predicate);
-
-
-    public async Task<Domain.Entities.Student> InsertAsync(Domain.Entities.Student entity)
-    {
-        await context.Set<Domain.Entities.Student>().AddAsync(entity);
-        await context.SaveChangesAsync();
-        return entity;
-    }
-    public async Task<Domain.Entities.Student> UpdateAsync(Domain.Entities.Student entity)
-    {
-        context.Update(entity);
-        await context.SaveChangesAsync();
-        return entity;
-    }
-    public async Task DeleteAsync(Domain.Entities.Student entity)
-    {
-        context.Remove(entity);
-        await context.SaveChangesAsync();
-    }
+        => await _context.Students.Include(q => q.Enrollments).ThenInclude(q => q.Course).SingleOrDefaultAsync(c => c.Id == id);
 }
