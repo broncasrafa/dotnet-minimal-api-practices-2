c083ceded4881c3316ec4739b485e4554b29a397
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 10:12:50 -0300

    👉🏾 updating Student entity

--- src/Student.Domain/Entities/Student.cs
+++ src/Student.Domain/Entities/Student.cs
@@ -6,7 +6,7 @@ public class Student : BaseEntity
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string NumberId { get; set; }
+    public string NumberId { get; set; } = Guid.NewGuid().ToString();
     public string Picture { get; set; }
     public DateTime DateofBirth { get; set; }
 }
