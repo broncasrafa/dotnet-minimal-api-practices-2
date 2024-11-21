ef4e860fe6e58b9c49a78d538640299d0ae4be9a
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 09:15:15 -0300

    👉🏾 adding automapper class profile

---
+++ src/Student.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Student.Application.DTO.Request;
+using Student.Application.DTO.Response;
+using Student.Domain.Entities;
+using AutoMapper;
+
+namespace Student.Application.Mappers;
+
+internal class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Course, CourseResponse>().ReverseMap();
+        CreateMap<CourseCreateRequest, Course>().ReverseMap();
+        CreateMap<CourseUpdateRequest, Course>().ReverseMap();
+    }
+}
