8f7d68554ad07e2005484ab4d5522dfc0514e2cb
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 10:36:56 -0300

    👉🏾 adding student repository class

--- src/Student.Domain/Interfaces/Repositories/IStudentRepository.cs
+++ src/Student.Domain/Interfaces/Repositories/IStudentRepository.cs
@@ -1,5 +1,17 @@
-﻿namespace Student.Domain.Interfaces.Repositories;
+﻿using System.Linq.Expressions;
+
+namespace Student.Domain.Interfaces.Repositories;
 
 public interface IStudentRepository
 {
+    Task<IEnumerable<Entities.Student>> GetAllAsync();
+    Task<Entities.Student> GetByIdAsync(int id);
+    Task<Entities.Student> GetStudentDetailsAsync(int id);
+    Task<Entities.Student> GetByAsync(Expression<Func<Entities.Student, bool>> predicate);
+    Task<IEnumerable<Entities.Student>> FindByAsync(Expression<Func<Entities.Student, bool>> predicate);
+    Task<Entities.Student> SingleOrDefaultAsync(Expression<Func<Entities.Student, bool>> predicate);
+    Task<Entities.Student> FirstOrDefaultAsync(Expression<Func<Entities.Student, bool>> predicate);
+    Task<Entities.Student> InsertAsync(Entities.Student request);
+    Task<Entities.Student> UpdateAsync(Entities.Student request);
+    Task DeleteAsync(Entities.Student entity);
 }
