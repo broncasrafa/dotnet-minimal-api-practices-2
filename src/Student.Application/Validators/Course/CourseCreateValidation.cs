f192e4f5486481bc6d06d7a9fd24b6494a7a5f2a
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 09:45:34 -0300

    👉🏾 adding course validations classes

---
+++ src/Student.Application/Validators/Course/CourseCreateValidation.cs
@@ -0,0 +1,23 @@
+﻿using Student.Application.Services.Interfaces;
+using Student.Application.DTO.Request;
+using FluentValidation;
+
+namespace Student.Application.Validators.Course;
+
+public class CourseCreateValidation : AbstractValidator<CourseCreateRequest>
+{
+    private readonly ICourseService _service;
+
+    public CourseCreateValidation(ICourseService courseService)
+    {
+        _service = courseService;
+
+        RuleFor(model => model.Title).Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("Course title is required")
+            .NotNull().WithMessage("Course title is required")
+            .MinimumLength(3).WithMessage($"Course title must at least 3 characters");
+
+        RuleFor(model => model.Credits).Cascade(CascadeMode.Stop)
+            .InclusiveBetween(1, 100).WithMessage("Course credits must be 1 between 100");
+    }
+}
---
+++ src/Student.Application/Validators/Course/CourseUpdateValidation.cs
@@ -0,0 +1,26 @@
+﻿using Student.Application.DTO.Request;
+using Student.Application.Services.Interfaces;
+using FluentValidation;
+
+namespace Student.Application.Validators.Course;
+
+public class CourseUpdateValidation : AbstractValidator<CourseUpdateRequest>
+{
+    private readonly ICourseService _courseService;
+    public CourseUpdateValidation(ICourseService courseService)
+    {
+        _courseService = courseService;
+
+        RuleFor(model => model.Id).Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("Course ID is required")
+            .GreaterThan(0).WithMessage("Course ID must be greater than 0");
+
+        RuleFor(model => model.Title).Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("Course title is required")
+            .NotNull().WithMessage("Course title is required")
+            .MinimumLength(3).WithMessage($"Course title must at least 3 characters");
+
+        RuleFor(model => model.Credits).Cascade(CascadeMode.Stop)
+            .InclusiveBetween(1, 100).WithMessage("Course credits must be 1 between 100");
+    }
+}
