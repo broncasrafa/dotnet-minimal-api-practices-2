9b2bf000272fa15e11da8656f6426ffa063c3a0e
Rafael Francisco <broncasrafa@gmail.com>
2024-11-21 10:12:04 -0300

    👉🏾 adding students dtos classes

---
+++ src/Student.Application/DTO/Request/StudentCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Student.Application.DTO.Request;
+
+public class StudentCreateRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string NumberId { get; set; }
+    public string Picture { get; set; }
+    public DateTime DateofBirth { get; set; }
+}
---
+++ src/Student.Application/DTO/Response/StudentDetailsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Student.Application.DTO.Response;
+
+public class StudentDetailsResponse : StudentResponse
+{
+    public List<CourseResponse> Courses { get; set; } = new List<CourseResponse>();
+}
